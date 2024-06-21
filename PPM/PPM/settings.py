550f4bc2c8becc39afe24ffdf18817f5b8232cf7
Aлзx <lionel.a913@gmail.com>
2024-06-21 11:49:31 +0200

    Update settings.py

--- PPM/PPM/settings.py
+++ PPM/PPM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)9znr6713nk$jb@0r)hpg_dc$roxfliz!(4erm4%)7w_=!%x#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
